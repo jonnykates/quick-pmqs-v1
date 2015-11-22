ecde5ea0e4604cee2afd6d8882047acd7331ecef
Jonny Kates <hi@jonnykates.com>
2015-11-22 17:43:48 +0000

    Date picker

--- js/controllers/TweetController.js
+++ js/controllers/TweetController.js
@@ -4,98 +4,11 @@ app.controller('TweetController', ['$scope', function($scope) {
 var corbynAv = 'img/corbyn-av.png';
 var cameronAv = 'img/cameron-av.png';
 
+$scope.debateSelector = {date: 'hideall'};
+
 $scope.debates = [
 
-  {
-    date: '21st October, 2015',
-
-    tweets: [
-      {
-        politician: 'Jeremy Corbyn',
-        avatar: corbynAv,
-        side: 'left',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew57',
-        message: 'Yesterday a Con. back bencher said working families will suffer from proposed cuts to tax credits. Where was she wrong?'
-      },
-      {
-        politician: 'David Cameron',
-        avatar: cameronAv,
-        side: 'right',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin13',
-        message: 'Changes to tax credits are part of a package that includes a higher living wage and tax reductions'
-      },
-      {
-        politician: 'Jeremy Corbyn',
-        avatar: corbynAv,
-        side: 'left',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew58',
-        message: 'Cuts to tax credits would particularly hurt the self-employed and small business owners. Shouldn\'t we help these people not punish them?'
-      },
-      {
-        politician: 'David Cameron',
-        avatar: cameronAv,
-        side: 'right',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin14',
-        message: 'People on the lowest incomes will continue to receive the child tax credit at £2,730'
-      },
-      {
-        politician: 'Jeremy Corbyn',
-        avatar: corbynAv,
-        side: 'left',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew59',
-        message: 'The PM said on TV before the election that tax credits would not be affected. Why the change of heart?'
-      },
-      {
-        politician: 'David Cameron',
-        avatar: cameronAv,
-        side: 'right',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin15',
-        message: 'We said we\'d reduce welfare by £12bn and we\'re keeping that promise.'
-      },
-      {
-        politician: 'Jeremy Corbyn',
-        avatar: corbynAv,
-        side: 'left',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew60',
-        message: 'Do you agree that the government\'s lack of intervention in the steel industry is having devestating effects on working people\'s lives?'
-      },
-      {
-        politician: 'David Cameron',
-        avatar: cameronAv,
-        side: 'right',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin16',
-        message: 'The steel industry is in a difficult situation globally due to supply & demand. We plan to take action to support steel in the UK'
-      },
-      {
-        politician: 'Jeremy Corbyn',
-        avatar: corbynAv,
-        side: 'left',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew61',
-        message: 'Thousands of steel jobs in the NE and midlands have been lost. We need serious action TODAY.'
-      },
-      {
-        politician: 'David Cameron',
-        avatar: cameronAv,
-        side: 'right',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin17',
-        message: 'We want a strong steel industry and will be taking action'
-      },
-      {
-        politician: 'Jeremy Corbyn',
-        avatar: corbynAv,
-        side: 'left',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew64',
-        message: 'The UN is investigating the UK for violations of disabled people\'s human rights. Will the PM cooperate with an open inquiry?'
-      },
-      {
-        politician: 'David Cameron',
-        avatar: cameronAv,
-        side: 'right',
-        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin19',
-        message: 'We\'ve helped many disabled people into work. I will look at UN report but often these are not all they are cracked up to be.'
-      }
-    ]
-  },
+  { date: 'Select a date' },
 
   {
     date: '28th October, 2015',
@@ -184,6 +97,97 @@ $scope.debates = [
         side: 'right',
         source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151028/debtext/151028-0001.htm#151028-0001.htm_spmin16',
         message: 'Families on a basic wage in the public sector will be able to earn £11k before tax + 30hrs childcare a week under this government.'
+      }
+    ]
+  },
+
+  {
+    date: '21st October, 2015',
+
+    tweets: [
+      {
+        politician: 'Jeremy Corbyn',
+        avatar: corbynAv,
+        side: 'left',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew57',
+        message: 'Yesterday a Con. back bencher said working families will suffer from proposed cuts to tax credits. Where was she wrong?'
+      },
+      {
+        politician: 'David Cameron',
+        avatar: cameronAv,
+        side: 'right',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin13',
+        message: 'Changes to tax credits are part of a package that includes a higher living wage and tax reductions'
+      },
+      {
+        politician: 'Jeremy Corbyn',
+        avatar: corbynAv,
+        side: 'left',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew58',
+        message: 'Cuts to tax credits would particularly hurt the self-employed and small business owners. Shouldn\'t we help these people not punish them?'
+      },
+      {
+        politician: 'David Cameron',
+        avatar: cameronAv,
+        side: 'right',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin14',
+        message: 'People on the lowest incomes will continue to receive the child tax credit at £2,730'
+      },
+      {
+        politician: 'Jeremy Corbyn',
+        avatar: corbynAv,
+        side: 'left',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew59',
+        message: 'The PM said on TV before the election that tax credits would not be affected. Why the change of heart?'
+      },
+      {
+        politician: 'David Cameron',
+        avatar: cameronAv,
+        side: 'right',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin15',
+        message: 'We said we\'d reduce welfare by £12bn and we\'re keeping that promise.'
+      },
+      {
+        politician: 'Jeremy Corbyn',
+        avatar: corbynAv,
+        side: 'left',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew60',
+        message: 'Do you agree that the government\'s lack of intervention in the steel industry is having devestating effects on working people\'s lives?'
+      },
+      {
+        politician: 'David Cameron',
+        avatar: cameronAv,
+        side: 'right',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin16',
+        message: 'The steel industry is in a difficult situation globally due to supply & demand. We plan to take action to support steel in the UK'
+      },
+      {
+        politician: 'Jeremy Corbyn',
+        avatar: corbynAv,
+        side: 'left',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew61',
+        message: 'Thousands of steel jobs in the NE and midlands have been lost. We need serious action TODAY.'
+      },
+      {
+        politician: 'David Cameron',
+        avatar: cameronAv,
+        side: 'right',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin17',
+        message: 'We want a strong steel industry and will be taking action'
+      },
+      {
+        politician: 'Jeremy Corbyn',
+        avatar: corbynAv,
+        side: 'left',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spnew64',
+        message: 'The UN is investigating the UK for violations of disabled people\'s human rights. Will the PM cooperate with an open inquiry?'
+      },
+      {
+        politician: 'David Cameron',
+        avatar: cameronAv,
+        side: 'right',
+        source: 'http://www.publications.parliament.uk/pa/cm201516/cmhansrd/cm151021/debtext/151021-0001.htm#151021-0001.htm_spmin19',
+        message: 'We\'ve helped many disabled people into work. I will look at UN report but often these are not all they are cracked up to be.'
       }
     ]
   }
